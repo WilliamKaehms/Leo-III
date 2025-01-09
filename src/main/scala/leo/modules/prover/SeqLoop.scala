b7df695e23c5b5eb61987c9507aa6fdfbc02251b
WilliamKaehms <william.kaehms@gmx.de>
2025-01-09 15:48:05 +0100

    Testing from TptpResult

--- src/main/scala/leo/modules/prover/SeqLoop.scala
+++ src/main/scala/leo/modules/prover/SeqLoop.scala
@@ -207,7 +207,7 @@ object SeqLoop {
               val emptyClause = AnnotatedClause(Clause.empty,
                 extCallInference(extResAnswer.prover.name,
                   extResAnswer.problem))
-              Out.comment("Some Testing with that!!!")
+              Out.comment(extResAnswer.output.mkString("\n"))
               endplay(emptyClause, state)
             } else {
               endplay(null, state)
